afead91052eecbca42a4abb841bde8337891e150
Loki <subtlepseudonym@gmail.com>
2018-07-25 01:03:16 -0400

    Add faster balance function and benchmarks

--- balance.go
+++ balance.go
@@ -28,6 +28,7 @@ func (s *BraceStack) Length() int {
 
 // Balance takes an input string and returns the index of the first unbalanced brace,
 // 0 indexed. If the input string is balanced, a -1 is returned
+// This function is O(n) compute and O(n) memory
 func Balance(in string) int {
 	var stack BraceStack
 	var braceIndex int
@@ -51,4 +52,38 @@ func Balance(in string) int {
 		return -1
 	}
 	return stack.braces[len(stack.braces)-1]
+}
+
+// FastBalance is a faster and more memory efficient, but less readable, implementation
+// of Balance
+// This function is O(n) compute and O(1) memory, though the computation time is
+// considerably faster than Balance. For small cases, FastBalance is ~3.5x faster and
+// for longer cases, can be anywhere between from 5x to 30x faster, depending upon how
+// flat (JSON-wise) the object formed by the input string is. Deeper objects see a greater
+// compute time improvement over Balance
+func FastBalance(in string) int {
+	var openCount, braceIndex int
+	leftmost := -1
+	for _, s := range []rune(in) {
+		switch s {
+		default:
+			continue
+		case '{':
+			if openCount == 0 {
+				leftmost = braceIndex
+			}
+			openCount++
+			braceIndex++
+		case '}':
+			if openCount == 1 {
+				leftmost = -1
+			} else if openCount == 0 {
+				return braceIndex
+			}
+			openCount--
+			braceIndex++
+		}
+	}
+
+	return leftmost
 }
